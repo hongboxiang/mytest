fb833fe44a1076e984f41cc082b42813f9ea24ee
洪博翔10180885 <hong.boxiang@zte.com.cn>
2017-03-13 11:11:15 +0800

    更新RFreport

--- writeRFreport.js
+++ writeRFreport.js
@@ -1,6 +1,7 @@
 var team = "11-ICM配置";
 var reason = "FAILED_TYPE_1";
 
+
 var first = document.getElementById("ids");
 var tr = first.parentNode.parentNode;
 var next = true;
@@ -13,11 +14,16 @@ while(next){
     var child = tr.childNodes;
     var icm = child[11].innerText;
     if(icm == team){
-        child[1].firstElementChild.checked  = true;
+        if(child[1].firstElementChild.checked == false){
+            if(child[19].childNodes[0].selectedOptions[0].innerText == "--请选择--"){
+                child[1].firstElementChild.checked  = true;
+            }
+        }
     }
     tr = tr.nextElementSibling;
 }
 
+
 $("#allfailedType").val(reason);
-$("#allfailedResult").attr("value",'物理环境问题');//填充内容
+$("#allfailedResult").attr("value",'环境问题');//填充内容
 $(':button').click();
