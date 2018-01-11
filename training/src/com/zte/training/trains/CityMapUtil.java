cd058f1cbc590f24d5451544822169453161cfda
洪博翔10180885 <hong.boxiang@zte.com.cn>
2018-01-11 15:29:03 +0800

    初步完成

---
+++ training/src/com/zte/training/trains/CityMapUtil.java
@@ -0,0 +1,7 @@
+package com.zte.training.trains;
+
+/**
+ * Created on 2018/1/11
+ */
+public class CityMapUtil {
+}
--- training/src/com/zte/training/trains/Graph.java
+++ training/src/com/zte/training/trains/Graph.java
@@ -3,26 +3,37 @@ package com.zte.training.trains;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Function;
 
 /**
  * Created on 2017/12/8
  */
 public class Graph {
 
-    public static final String NO_SUCH_ROUTE = "NO SUCH ROUTE";
-    private final List<Route> routeList = new ArrayList<>();
+    static final String NO_SUCH_ROUTE = "NO SUCH ROUTE";
+    private static int cityAmount = 5;
+    private static int[][] cityMap;
+    private List<String> trips = new ArrayList<>();
+    private int bestCost;
+    private String bestPath;
 
-    public Graph(List<String> routeStrings) {
-        routeStrings.forEach(routeString ->routeList.add(new Route(routeString)));
+    public Graph(int cityAmount, List<String> routeStrings) {
+        this.cityAmount = cityAmount;
+        initCityMap(cityAmount, routeStrings);
+    }
+
+    private void initCityMap(int cityAmount, List<String> routeStrings) {
+        cityMap = new int[cityAmount][cityAmount];
+        for (int i = 0; i < cityAmount; i++) {
+            Arrays.fill(cityMap[i], -1);
+        }
+        routeStrings.forEach(routeString ->cityMap[(int)routeString.charAt(0)-'A'][(int)routeString.charAt(1)-'A'] = Integer.parseInt(routeString.substring(2, 3)));
     }
 
     public String routeDistance(String ... points) {
         int distancesum=0;
         for(int index=0; index<points.length-1; index++){
-            int finalIndex = index;
-            int distance = distanceFromStoD(points[finalIndex], points[finalIndex+1]);
-            if(distance == 0){
+            int distance = distanceFromStoD(points[index], points[index +1]);
+            if(distance == -1){
                 return NO_SUCH_ROUTE;
             }
             distancesum += distance;
@@ -31,15 +42,110 @@ public class Graph {
     }
 
     private Integer distanceFromStoD(String start, String des) {
-        return routeList.stream().filter(route -> start.equals(route.getStartPoint()) &&
-                des.equals(route.getDesPoint()))
-                .map(route -> route.getDistance()).reduce(0, Integer::sum);
+        return cityMap[(int)start.charAt(0)-'A'][(int)des.charAt(0)-'A'];
     }
 
-//    public String tripsNum(String start, String end, int stepnum){
-//        while (stepnum>0){
-//            stepnum--;
-//            if(start)
-//        }
-//    }
+    public int calTripsNum(String start, String end, int stepnum){
+        trips.clear();
+        tripsNumMaxStep(end, start, stepnum);
+        System.out.println(trips);
+        return trips.size();
+    }
+
+    public int calTripsNumExactly(String start, String end, int stepnum){
+        trips.clear();
+        tripsNumExactlyStep(end, start, stepnum);
+        System.out.println(trips);
+        return trips.size();
+    }
+
+    public int shortestRoute(String start, String end){
+        bestPath = "";
+        bestCost = Integer.MAX_VALUE;
+        dfs(end, start, 0);
+        System.out.println(bestPath);
+        return bestCost;
+    }
+
+    public int calTripsNumDiffRoute(String start, String end, int cost){
+        trips.clear();
+        tripsNumMaxStepDiffRoute(end, start, 0);
+        System.out.println(trips);
+        return trips.size();
+    }
+
+    private void tripsNumMaxStepDiffRoute(String end, String path, int cost){
+        if (cost >= 30)
+            return;
+        if (cost > 0 && path.endsWith(end)) {
+            trips.add(path);
+        }
+        char lastChar = path.charAt(path.length() - 1);
+        int lastNodeIndex = lastChar - 'A';
+        for (int i = 0; i < cityMap[lastNodeIndex].length; i++) {
+            char newChar = (char) (i + 'A');
+            int newCost = distanceFromStoD(String.valueOf(lastChar), String.valueOf(newChar));
+            if (newCost > -1) {
+                tripsNumMaxStepDiffRoute(end, path + newChar, cost + newCost);
+            }
+        }
+    }
+
+    private void dfs(String end, String path, int cost){
+        if (path.endsWith(end) && cost < bestCost && cost > 0) {
+            bestPath = path;
+            bestCost = cost;
+            return;
+        }
+        char lastChar = path.charAt(path.length()-1);
+
+        for (int i = 0; i< cityAmount; i++ )
+        {
+            char newChar = (char)(i + 'A');
+            int newCost = distanceFromStoD(String.valueOf(lastChar), String.valueOf(newChar));
+            if(newCost>-1){
+                if(path.indexOf(newChar)>0){
+                    continue;
+                }
+            dfs(end, path + newChar, cost + newCost);
+            }
+        }
+    }
+
+    private void tripsNumMaxStep(String end, String path, int stepnum){
+        if (path.length() - 1 > stepnum) return;
+        if ( path.length() > 1 && path.endsWith(end) ) {
+            trips.add(path);
+        }
+        char lastChar = path.charAt(path.length()-1);
+
+        for (int i = 0; i< cityAmount; i++ )
+        {
+            char newChar = (char)(i + 'A');
+            if(isHaveRoute(String.valueOf(lastChar), String.valueOf(newChar))){
+                tripsNumMaxStep(end, path + newChar, stepnum);
+            }
+        }
+    }
+
+    private void tripsNumExactlyStep(String end, String path, int stepnum){
+        if (path.length() - 1 > stepnum) return;
+        if (path.length() - 1 == stepnum && path.endsWith(end) ) {
+            trips.add(path);
+            return;
+        }
+        char lastChar = path.charAt(path.length()-1);
+
+        for ( int i=0; i<cityAmount; i++ )
+        {
+            char newChar = (char)(i + 'A');
+            if(isHaveRoute(String.valueOf(lastChar), String.valueOf(newChar))){
+                tripsNumExactlyStep(end, path + newChar, stepnum);
+            }
+        }
+    }
+
+    private boolean isHaveRoute(String start, String des) {
+        return cityMap[(int)start.charAt(0)-'A'][(int)des.charAt(0)-'A'] > -1;
+    }
 }
